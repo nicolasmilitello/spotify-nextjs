5ac442028510531e042f40bc94c2ab949df544ea
nicolasmilitello <nmilitello@applaudostudios.dev>
2022-10-09 19:29:36 -0300

    feat: create a content separator component

---
+++ components/content-separator/content-separator.tsx
@@ -0,0 +1,9 @@
+const ContentSeparator = () => {
+	return (
+		<div className="flex justify-center py-5 items-center w-full">
+			<div className="border-t border-gray-400 w-2/4"></div>
+		</div>
+	);
+};
+
+export default ContentSeparator;
